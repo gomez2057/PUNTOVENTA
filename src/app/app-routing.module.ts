dcff5757269368a96ae89c9d41edd081e11b5ef1
gomez2057 <144506410+gomez2057@users.noreply.github.com>
2023-09-08 22:20:46 -0500

    PRIMER PUSH

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {path:'', component:LoginComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
